ff98465a6cefa27a83eded2f376818af67a5f6f9
jaabarri <jaabarri1112@gmail.com>
2023-11-28 11:05:29 -0800

    Functioning base website with map api

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function redirectToAnotherPage() {
+    // Specify the URL of the target webpage
+    var targetUrl = '../maps/index.html';
+
+    // Redirect to the target webpage
+    window.location.href = targetUrl;
+}
