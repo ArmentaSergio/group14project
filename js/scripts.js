7a902c4645100710ecb525688068a4895bf7abf3
Jaabarri12 <105237423+Jaabarri12@users.noreply.github.com>
2023-12-08 15:22:53 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
-function redirectToAnotherPage() {
-    // Specify the URL of the target webpage
-    var targetUrl = '../maps/index.html';
-
-    // Redirect to the target webpage
-    window.location.href = targetUrl;
-}
+$(document).ready(function() {
+    // When a tab is clicked
+    $('.tab').click(function() {
+        // Get the value of the data-tab attribute of the clicked tab
+        var tabId = $(this).data('tab');
+        console.log("Tab clicked:", tabId);
+        
+        // Remove the 'active' class from all tabs
+        $(".tab").removeClass('active');
+        // Add the 'active' class to the clicked tab
+        $(this).addClass('active');
+        
+        // Hide all tab panes
+        $('.tab-pane').removeClass('active');
+        // Show the tab pane corresponding to the clicked tab
+        $('[data-tab-content="' + tabId + '"]').addClass('active');
+    });
+});
